968784020e2f7e1cce3184a6ed3daef2bab0ee17
Eric Cochran <eric@nightlynexus.com>
2018-02-20 15:16:02 -0800

    Update to KotlinPoet 0.7.
    and its double indent. *sniff*

--- quine/src/main/kotlin/com/nightlynexus/kotlinpoetquine/Quine.kt
+++ quine/src/main/kotlin/com/nightlynexus/kotlinpoetquine/Quine.kt
@@ -17,39 +17,39 @@ private const val PACKAGE_NAME: String = "com.nightlynexus.kotlinpoetquine"
 private const val FILE_NAME: String = "Quine"
 
 fun main(args: Array<String>) {
-  val string = "%S"
-  val body = """
-  |val string = %S
-  |val body = %S
-  |%T.builder(PACKAGE_NAME, FILE_NAME)
-  |    .addStaticImport("com.squareup.kotlinpoet", "ARRAY")
-  |    .addStaticImport("com.squareup.kotlinpoet", "asClassName")
-  |    .addStaticImport(%T::class, "CONST")
-  |    .addStaticImport(KModifier::class, "PRIVATE")
-  |    .addProperty(%T.builder("PACKAGE_NAME", String::class, PRIVATE, CONST)
-  |        .initializer(string, PACKAGE_NAME).build())
-  |    .addProperty(PropertySpec.builder("FILE_NAME", String::class, PRIVATE, CONST)
-  |        .initializer(string, FILE_NAME).build())
-  |    .addFunction(%T.builder("main").addParameter(
-  |        "args", %T.get(ARRAY, String::class.asClassName()))
-  |        .addCode(body, string, body, FileSpec::class, KModifier::class, PropertySpec::class,
-  |            FunSpec::class, ParameterizedTypeName::class)
-  |        .build())
-  |    .build().writeTo(System.out)
-  |""".trimMargin()
-  FileSpec.builder(PACKAGE_NAME, FILE_NAME)
-      .addStaticImport("com.squareup.kotlinpoet", "ARRAY")
-      .addStaticImport("com.squareup.kotlinpoet", "asClassName")
-      .addStaticImport(KModifier::class, "CONST")
-      .addStaticImport(KModifier::class, "PRIVATE")
-      .addProperty(PropertySpec.builder("PACKAGE_NAME", String::class, PRIVATE, CONST)
-          .initializer(string, PACKAGE_NAME).build())
-      .addProperty(PropertySpec.builder("FILE_NAME", String::class, PRIVATE, CONST)
-          .initializer(string, FILE_NAME).build())
-      .addFunction(FunSpec.builder("main").addParameter(
-          "args", ParameterizedTypeName.get(ARRAY, String::class.asClassName()))
-          .addCode(body, string, body, FileSpec::class, KModifier::class, PropertySpec::class,
-              FunSpec::class, ParameterizedTypeName::class)
-          .build())
-      .build().writeTo(System.out)
+    val string = "%S"
+    val body = """
+    |val string = %S
+    |val body = %S
+    |%T.builder(PACKAGE_NAME, FILE_NAME)
+    |    .addStaticImport("com.squareup.kotlinpoet", "ARRAY")
+    |    .addStaticImport("com.squareup.kotlinpoet", "asClassName")
+    |    .addStaticImport(%T::class, "CONST")
+    |    .addStaticImport(KModifier::class, "PRIVATE")
+    |    .addProperty(%T.builder("PACKAGE_NAME", String::class, PRIVATE, CONST)
+    |        .initializer(string, PACKAGE_NAME).build())
+    |    .addProperty(PropertySpec.builder("FILE_NAME", String::class, PRIVATE, CONST)
+    |        .initializer(string, FILE_NAME).build())
+    |    .addFunction(%T.builder("main").addParameter(
+    |        "args", %T.get(ARRAY, String::class.asClassName()))
+    |        .addCode(body, string, body, FileSpec::class, KModifier::class, PropertySpec::class,
+    |            FunSpec::class, ParameterizedTypeName::class)
+    |        .build())
+    |    .build().writeTo(System.out)
+    |""".trimMargin()
+    FileSpec.builder(PACKAGE_NAME, FILE_NAME)
+        .addStaticImport("com.squareup.kotlinpoet", "ARRAY")
+        .addStaticImport("com.squareup.kotlinpoet", "asClassName")
+        .addStaticImport(KModifier::class, "CONST")
+        .addStaticImport(KModifier::class, "PRIVATE")
+        .addProperty(PropertySpec.builder("PACKAGE_NAME", String::class, PRIVATE, CONST)
+            .initializer(string, PACKAGE_NAME).build())
+        .addProperty(PropertySpec.builder("FILE_NAME", String::class, PRIVATE, CONST)
+            .initializer(string, FILE_NAME).build())
+        .addFunction(FunSpec.builder("main").addParameter(
+            "args", ParameterizedTypeName.get(ARRAY, String::class.asClassName()))
+            .addCode(body, string, body, FileSpec::class, KModifier::class, PropertySpec::class,
+                FunSpec::class, ParameterizedTypeName::class)
+            .build())
+        .build().writeTo(System.out)
 }
